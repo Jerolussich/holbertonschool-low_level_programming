6497158e1532612dfba22e0b9d6a263581fa63a6
Jerolussich <jerolussich@hotmail.com>
2022-09-27 10:34:39 -0700

    task 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,13 +2,10 @@
 
 /**
  * main- Entry point
- * 
+ *
  * Description: Print number
  * Return: Return
  */
-
-int main(void)
-
 {
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
