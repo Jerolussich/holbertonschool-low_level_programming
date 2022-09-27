8dba6598a858474d4354281f7e52331597ebc9d4
Jerolussich <jerolussich@hotmail.com>
2022-09-27 10:18:39 -0700

    task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+ 
+/**
+ * main: void
+ *
+ * Description: A code using printf
+ */ 
+ 
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,")
+}
+
+	return(0)
