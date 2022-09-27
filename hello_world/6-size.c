b66e10a4b5567653a63c3e2364246d80a2119a69
Jerolussich <jerolussich@hotmail.com>
2022-09-27 11:41:30 -0700

    task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -16,10 +16,10 @@ int main(void)
 	long long int tripleType;
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
-	printf("Size of an int: %ld bytes(s)\n", sizeof(integerType));
-	printf("Size of a long int: %ld bytes(s)\n", sizeof(doubleType));
+	printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(doubleType));
 	printf("Size of a long long %ld byte(s)\n", sizeof(tripleType));
-	printf("Size of a float: %ld bytes(s)\n", sizeof(floatType));
+	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 
 	return (0);
 }
