40239680c622f610fab2b5fa408d129686e09c4a
Jerolussich <jerolussich@hotmail.com>
2022-09-27 10:04:31 -0700

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
-int main(void)
-{
+
 /**
- * Main- Entry point
+ * main- Entry point
+ *
+ * Description: Print number
+ * Return: Return
  */
-}	
+
+int main(void)
+
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
+
+
