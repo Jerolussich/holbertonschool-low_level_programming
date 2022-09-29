d4845a052d347138e8bbebc7d1895ae72a2af3d5
Jerolussich <jerolussich@hotmail.com>
2022-09-29 11:09:07 -0700

    task 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,19 +7,13 @@
  */
 
 int main(void)
-
 {
-
 	char n;
 
-	for (n = '0'; n <= 'f'; n++)
-	{
-		if ('9' < n <= '`';)
-
-		else
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	for (n = 'a'; n <= 'f'; n++)	
 		putchar(n)
-
-	}
 
 	putchar('\n');
 
