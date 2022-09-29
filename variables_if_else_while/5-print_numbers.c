cbde247c99586c94f4d752f8981cbca02e5a3655
Jerolussich <jerolussich@hotmail.com>
2022-09-29 07:09:07 -0700

    task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include  <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - void
+ * Return: all single digits
+ */
+
+	int main(void)
+
+{
+
+	char n = '0';
+
+	for (n = '0'; n <= '9'; n++)
+
+	{
+	putchar('n');
+	}
+
+	return (0);
+}
