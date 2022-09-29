3b9f00c8708538a9f80a89b88f34f60f04968049
Jerolussich <jerolussich@hotmail.com>
2022-09-29 06:23:11 -0700

    task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include  <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - void
+ * Return: Alphabet
+ */
+
+	int main(void)
+
+{
+
+	char ch = 'a';
+	char CH = 'A';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	{
+	if (ch != 'e' || ch != 'q')
+
+		putchar(ch);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
