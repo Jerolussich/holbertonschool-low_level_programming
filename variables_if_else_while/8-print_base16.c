324915a45b124bee0bd06f976c352e3178b32700
Jerolussich <jerolussich@hotmail.com>
2022-09-29 10:21:54 -0700

    task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - void
+ * Return: all base numbers of 16
+ */
+
+int main(void)
+
+{
+
+	char n;
+
+	for (n = '0'; n <= '15'; n++)
+
+	putchar(n);
+
+	return (0);
+}
