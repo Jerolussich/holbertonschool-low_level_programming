680d24e4b722a6703a6429d0a80db74f91a02896
Jerolussich <jerolussich@hotmail.com>
2022-09-29 09:57:05 -0700

    task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include  <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - void
+ * Return: Alphabet
+ */
+
+int main(void)
+
+{
+
+	char ch = "z"
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+
+	putchar(ch);
+
+
+
+	putchar('\n');
+
+	return (0);
+}
