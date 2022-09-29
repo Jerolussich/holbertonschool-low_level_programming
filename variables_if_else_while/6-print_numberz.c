bdf5e48e5fdf0f418395e61ded907d141843df82
Jerolussich <jerolussich@hotmail.com>
2022-09-29 08:46:42 -0700

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - void
+ * Return: all single digits
+ */
+
+	int main(void)
+
+{
+
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+
+	putchar(n);
+	printf("\n");
+
+	return (0);
+}
