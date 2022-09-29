d10a6d815a877c3e24412efb3ad8ff36ab6b07cb
Jerolussich <jerolussich@hotmail.com>
2022-09-29 11:30:17 -0700

    task 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - void
+ * Return: all base numbers of 16
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 0; n <= 9; n++)
+
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+
+	putchar('\n');
+
+	return (0);
+}
