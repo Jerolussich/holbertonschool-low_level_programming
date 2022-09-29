09c3b5f8a66e7ed77d9c18407d29051fe07fe2f1
Jerolussich <jerolussich@hotmail.com>
2022-09-29 05:49:55 -0700

    task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include  <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - void
+ * Return: Alphabet
+ */
+
+	int main(void)
+
+{
+
+	char ch = 'a';
+	char CH = 'A';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	{
+		putchar(ch);
+	}
+
+		for (CH = 'A'; CH <= 'Z'; CH++)
+
+		{
+			putchar(CH);
+		}
+
+
+	putchar('\n');
+
+	return (0);
+}
