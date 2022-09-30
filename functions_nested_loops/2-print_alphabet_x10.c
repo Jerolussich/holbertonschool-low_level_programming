02f1671437d9875fcd96d8d3968bd6655ddbaea9
Jerolussich <jerolussich@hotmail.com>
2022-09-30 11:47:25 -0700

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+
+{
+
+	char ch;
+	int t;
+
+	for (t = "0"; t <= "10"; t++)
+
+	{
+
+	if (ch = 'a'; ch <= 'z'; ch++)
+
+		_putchar(ch);
+	}
+
+		_putchar('\n');
+
+	return (0);
+}
