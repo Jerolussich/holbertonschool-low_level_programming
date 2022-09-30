37c132e0e4bfdde2e5b0fa4c0511e4630d7d841f
Jerolussich <jerolussich@hotmail.com>
2022-09-30 16:33:31 -0700

    task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _islower(int c) - Check lowercase
+ * Description: Check loewrcase
+ *
+ */
+
+int _islower(int c)
+	
+	{
+	
+	char ch
+	
+	if (ch >= '65' && ch <= '90')
+		
+		return (0);
+
+
