5f0dec6396106429eb419c48281ef16d833fe5e0
Jerolussich <jerolussich@hotmail.com>
2022-09-30 11:07:37 -0700

    task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -11,14 +11,14 @@ void print_alphabet(void)
 	char a;
 
 
-	for ( a = 'a', 'a' <= 'z', a++)
+	for ( a = 'a'; 'a' <= 'z'; a++)
 	{
 
-	_putchar('a');
+		_putchar('a');
 
 	}
 
 	_putchar('\n');
 
-	return (0);
+	return;
 }
