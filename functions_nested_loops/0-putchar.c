37dc95044176a053ad5855f01b322104f223678a
Jerolussich <jerolussich@hotmail.com>
2022-09-30 08:42:42 -0700

    task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,11 +2,11 @@
 
 int main()
 {
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
 	_putchar('c');
-	_putchar('c');
-	_putchar('c');
-	_putchar('c');
-	_putchar('c');
-	_putchar('c');
-	_putchar('c');
-	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
