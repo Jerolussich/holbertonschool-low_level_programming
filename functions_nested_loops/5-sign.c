d84139e2deefe8db58d84cdbc71cd372a5e61ec5
Jerolussich <jerolussich@hotmail.com>
2022-09-30 19:26:37 -0700

    task 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -13,13 +13,13 @@ int print_sign(int n)
 
 	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
 		_putchar(n);
 		return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar(45);
+		_putchar('-');
 		_putchar(n);
 		return (-1);
 	}
