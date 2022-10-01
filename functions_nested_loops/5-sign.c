51fb91eca0880afaba1295f55c35e48378a77330
Jerolussich <jerolussich@hotmail.com>
2022-09-30 19:33:39 -0700

    task 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * print_sign - print the sign of a number
  * @n: variable that stores value
  * Description: Print the sign of a number
- * Return: Sign of numbers and returns 
+ * Return: Sign of numbers and returns
  */
 
 
@@ -14,21 +14,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(n);
 		return (1);
 	}
 	else if (n < 0)
 	{	
-		_putchar('-');
-		_putchar(n);
+		_putchar('-')
 		return (-1);
 	}
 	else
 	{	
 		_putchar('0');
-		_putchar(n);
 		return (0);
 	}	
 
 }
-
