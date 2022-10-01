58025a5196e3ea8f48b2ef5779240d037667e014
Jerolussich <jerolussich@hotmail.com>
2022-09-30 19:12:51 -0700

    task 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a number
+ * @n: variable that stores value
+ * Description: Print the sign of a number
+ * Return: Sign of numbers and returns 
+ */
+
+
+int print_sign(int n)
+{
+
+	if (c > 0)
+		
+		_putchar('+');
+		_putchar(',');
+		_putchar(n);
+		return (1);
+
+	if (c < 0)
+		_putchar('-');
+		_putchar(',');
+		_putchar(n);
+		return (-1);
+
+	else
+		_putchar(0);
+		_putchar(',');
+		_putchar(n);
+		return (0);
+		
+
+}
+
