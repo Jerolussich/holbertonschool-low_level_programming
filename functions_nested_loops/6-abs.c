e084cc162f53cd4ea664813616fefa1678a0c6ce
Jerolussich <jerolussich@hotmail.com>
2022-09-30 19:46:35 -0700

    task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int_abs - print the sign of a number
+ * @int: variable that stores value
+ * Description: Print absolute number
+ * Return: absolute number
+ */
+
+
+int int_abs(int)
+{
+
+	abs(int);	
+}
