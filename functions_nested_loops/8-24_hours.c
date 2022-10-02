e6ae85fe6a0445b4b7173153ca112f32f465489e
Jerolussich <jerolussich@hotmail.com>
2022-10-01 20:47:35 -0700

    task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * jack_bauer - print all minutes of the day
+ * @n: variable that stores value
+ * Description: Print last digit of a number
+ * Return: last digit
+ */
+
+
+void jack_bauer(void)
+{
+	int H = 0
+	int M = 0
+
+	while (H < 23)
+	{
+	M = 0;	
+		
+		while (M < 60)
+		{
+		if(H<10){
+		_putchar('0');
+		}	
+		_putchar(H);
+		_putchar(':')
+		if (M < 10)
+		{
+		_putchar('0');
+		}
+		_putchar(M);
+		M++;
+		}
+	H++;
+
+	}
+]
