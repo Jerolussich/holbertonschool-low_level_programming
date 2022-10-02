b7cf952785b42043b7d4441273b57c7e98577565
Jerolussich <jerolussich@hotmail.com>
2022-10-01 21:33:05 -0700

    task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -19,13 +19,13 @@ void jack_bauer(void)
 		while (M < 60)
 		{
 		if (H < 10){
-		_putchar((H / 10) + '0');
+		_putchar('0');
 		}	
 		_putchar((H % 10) + '0');
 		_putchar(':');
 		if (M < 10)
 		{
-		_putchar((M / 10) + '0');
+		_putchar('0');
 		}
 		_putchar((M % 10) + '0');
 		_putchar('\n');
