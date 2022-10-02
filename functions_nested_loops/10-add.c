b814ce9c44d3275f39707c3fb2ae66575b04e2a8
Jerolussich <jerolussich@hotmail.com>
2022-10-02 10:59:36 -0700

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - sum number
+ * Description: adds two integers
+ * Return: sum of integer
+ */
+
+
+int add(int a, int b)
+{
+	int ab= a + b;
+
+	_putchar(ab);
+
+
+
+}
