726df91d8843acc8b3f4bc5171bc3bfb03372a73
Jerolussich <jerolussich@hotmail.com>
2022-10-02 11:32:56 -0700

    task 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - sum number
+ * Description: print all nat num
+ * Return: int from n to 98
+ */
+
+
+void print_to_98(int n)
+	
+	int n
+
+	if (n > 0 && 98 < n)
+		
+		printf("%d", n);
+
+}
