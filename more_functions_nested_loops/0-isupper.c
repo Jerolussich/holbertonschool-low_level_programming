07cb5ad5f745ecc201e8a85d90c7ca9c48d6674f
Jerolussich <jerolussich@hotmail.com>
2022-10-03 11:17:39 -0700

    task 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - check lower and upper case
+ * @C: variable that stores value
+ * Description: check lower and upper case
+ * Return: 1 lowercase, 0 othercase
+ **/
+
+int _isupper(int c)
+{
+	if (c >= 97 && 122 >= c)
+
+		return (1);
+	else
+		Return (0);
