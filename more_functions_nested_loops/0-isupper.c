527d43293f263d56bbbf7186e80945f21899041f
Jerolussich <jerolussich@hotmail.com>
2022-10-03 11:36:01 -0700

    task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * _isupper - check lower and upper case
- * @C: variable that stores value
  * Description: check lower and upper case
  * Return: 1 lowercase, 0 othercase
  **/
@@ -9,9 +8,9 @@
 int _isupper(int c)
 {
 	if (c >= 65 && 90 >= c)
-	
+
 		return (1);
-	
+
 
 	else
 		return (0);
