2eb5492e869d0fd461996d477f3bdbd925bfcfbd
Jerolussich <jerolussich@hotmail.com>
2022-10-04 04:27:47 -0700

    task 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - check digit or otherwise
+ * @c: variable that stores value
+ * Description: check digit or nondigit
+ * Return: 1 digit, 0 othercase
+ **/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && 57 >= c)
+
+		return (1);
+
+
+	else
+		return (0);
+}
