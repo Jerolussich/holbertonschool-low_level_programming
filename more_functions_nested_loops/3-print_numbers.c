b79aa227abee5f12c610f2b004bcb2c555ccfe1c
Jerolussich <jerolussich@hotmail.com>
2022-10-04 04:44:42 -0700

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print from 0 to 9
+ * @n: variable that stores value
+ * Description: prunt numbers
+ * Return: 0
+ **/
+
+void print_numbers(void)
+{
+	int n
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n)
+		_putchar('\n')
+	}
+}
