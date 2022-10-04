4927e1b6c66b32f38ff23fa5f30b9e2f6f903e65
Jerolussich <jerolussich@hotmail.com>
2022-10-04 06:58:47 -0700

    tasl 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_line - print from 0 to 14, 14 times
+ * @n: variable that stores value
+ * Description: print numbers 14 times
+ * Return: 0
+ **/
+
+void print_line(int n)
+{
+	int p = 0;
+
+	while (p < n)
+	{
+		if (n > p)
+		{
+		_putchar('92');
+		p++;
+
+		}
+	{
+
+	_putchar('\n');
+
+	}
+}
