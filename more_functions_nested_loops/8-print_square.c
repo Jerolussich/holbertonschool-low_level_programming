db6b4162d323fe3c0f00e3f27eb0140e85ecb29e
Jerolussich <jerolussich@hotmail.com>
2022-10-04 09:47:26 -0700

    task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_square - print diagonal with spaces
+ * @n: variable that stores value
+ * Description: print numbers 14 times
+ * Return: 0
+ **/
+
+void print_square(int size)
+{
+	int p = 0;
+	int s = 0;
+	int i = 0;
+
+	while (i < n)
+	{
+
+		while (n > p)
+		{
+			_putchar('#');
+			p++;
+		}
+	i++;
+
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+}
