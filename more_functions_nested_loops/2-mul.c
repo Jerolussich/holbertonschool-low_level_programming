4657e2005345ca9cef68d1513758853a0af37ff6
Jerolussich <jerolussich@hotmail.com>
2022-10-04 04:32:08 -0700

    task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiply two integers
+ * @c: variable that stores value
+ * Description: multiply two integers
+ * Return: 0
+ **/
+
+int mul(int a, int b)
+{
+	int ab = a * b;
+
+	return (0);
+}
