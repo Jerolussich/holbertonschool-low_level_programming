7a98a0a96cb911e2e5c35ea7d5bda6b7b474c12c
Jerolussich <jerolussich@hotmail.com>
2022-10-04 05:10:21 -0700

    task 4

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
  * print_numbers - print from 0 to 9
- * @n: variable that stores value
- * Description: prunt numbers
+ * Description: print numbers
  * Return: 0
  **/
 
@@ -10,10 +9,10 @@ void print_numbers(void)
 {
 	int n = '0';
 
-	for (; n < 10; n++)
+	for (; 'n' < '10'; n++)
 	{
 		_putchar(n);
 	}
 		_putchar('\n');
-	
+
 }
