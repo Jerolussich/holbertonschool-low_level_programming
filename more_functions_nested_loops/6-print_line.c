52204a46c6281329a856371321285cf8b8d4369a
Jerolussich <jerolussich@hotmail.com>
2022-10-04 06:38:55 -0700

    task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * more_numbers - print from 0 to 14, 14 times
+ * @n: variable that stores value
+ * @I: variable that stores value
+ * Description: print numbers 14 times
+ * Return: 0
+ **/
+
+void print_line(int n)
+{
+	int n
+	int p= 0;
+
+	while (p < n)
+	{	
+		if (n > p)
+		{	
+		_putchar('_');
+		p++
+
+		}
+
+		_putchar('\n');
+
+	}
+}
