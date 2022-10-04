c659d285a1b62e15f0a9d856671c5f3451dd56f9
Jerolussich <jerolussich@hotmail.com>
2022-10-04 07:05:14 -0700

    task 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * print_numbers - print from 0 to 9
- * @n: variable that stores value
+ * print_most_numbers - print from 0 to 9
  * Description: prunt numbers
  * Return: 0
  **/
@@ -11,8 +10,7 @@ void print_most_numbers(void)
 	int n = 0;
 
 	for (; n < 10; n++)
-	{
-		if (n != 2 && 4 != n)
+
 		{
 			_putchar(n + '0');
 		}
