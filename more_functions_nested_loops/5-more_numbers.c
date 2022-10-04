28398b525783a318d5f13b60aafeb245e2ea0c84
Jerolussich <jerolussich@hotmail.com>
2022-10-04 05:42:14 -0700

    task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * print_numbers - print from 0 to 9
+ * more_numbers - print from 0 to 14, 14 times
  * @n: variable that stores value
- * Description: prunt numbers
+ * @I: variable that stores value
+ * Description: print numbers 14 times
  * Return: 0
  **/
 
-void print_most_numbers(void)
+void more_numbers(void)
 {
 	int n = 0;
-	int m = 0;
 	int i = 0;
 
 	while (i < 14)
 	{
-		n = 0
+		n = 0;
 			for (; n < 14; n++)
 			{
 				_putchar(n + '0');
