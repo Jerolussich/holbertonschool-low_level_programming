ce80551376dde5f48e650c20b7c4b6695751f6bf
Jerolussich <jerolussich@hotmail.com>
2022-10-04 05:40:08 -0700

    task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_numbers - print from 0 to 9
+ * @n: variable that stores value
+ * Description: prunt numbers
+ * Return: 0
+ **/
+
+void print_most_numbers(void)
+{
+	int n = 0;
+	int m = 0;
+	int i = 0;
+
+	while (i < 14)
+	{
+		n = 0
+			for (; n < 14; n++)
+			{
+				_putchar(n + '0');
+				n++;
+			}
+		i++;
+		_putchar('\n');
+
+	}
+}
