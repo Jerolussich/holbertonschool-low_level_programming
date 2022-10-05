ef6c5b4c9a35038f0000a9c25726ba72e38eb24b
Jerolussich <jerolussich@hotmail.com>
2022-10-05 06:04:48 -0700

    task 0

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <studio.h>
+/**
+ * main - print 1 to 100
+ * Description: print 1 to 100
+ **/
+
+int main(void)
+
+{
+	while (n > 1 && 100 > n)
+	
+		if (i % 3 == 0)
+			printf("fizz ", n);
+			n++;
+		if (i % 5 == 0)
+			printf("Buzz ", n);
+			n++;
+		if (i % 15 == 0)
+			printf("fizzbuzz ", n);
+		else
+			printf("%d ", n);
