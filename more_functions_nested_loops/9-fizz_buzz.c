ef79167fe5b1376a6fe95c9b1da9d813702d3486
Jerolussich <jerolussich@hotmail.com>
2022-10-05 07:03:25 -0700

    task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -3,34 +3,36 @@
 /**
  * main - print 1 to 100
  * Description: print 1 to 100
+ * return: numbers 1 to 100
  **/
 
 int main(void)
-
 {
 
 	int n;
 
 	for (n = 1; n <= 100; n++)
 	{
-	
+
 		if (n % 15 == 0)
 		{
-			printf("FizzBuzz ");	
+			printf("FizzBuzz");
 		}
 		else if (n % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
 			printf("%d ", n);
-			
+
 		}
+	printf(" ");
 	}
+
 	return (0);
 }
