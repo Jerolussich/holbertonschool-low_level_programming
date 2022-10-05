8c994d274d5627eaf2771638d71a99f57813e2f9
Jerolussich <jerolussich@hotmail.com>
2022-10-05 06:13:43 -0700

    task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,21 @@ int main(int n)
 	while (n > 1 && 100 > n)
 	
 		if (n % 3 == 0)
+		{
 			printf("fizz ", n);
 			n++;
+		}
 		if (n % 5 == 0)
+		{
 			printf("Buzz ", n);
 			n++;
+		}
 		if (n % 15 == 0)
+		{
 			printf("fizzbuzz ", n);
+		}
 		else
+		{
 			printf("%d ", n);
+		}
 }
