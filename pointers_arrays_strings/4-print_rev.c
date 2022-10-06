8898c71f1a8a1b3476438a6be5e36691bc9bcc3f
Jerolussich <jerolussich@hotmail.com>
2022-10-06 09:04:55 -0700

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _puts - print char
+ * @str: stores a value
+ * Return: returns sums of string
+ **/
+
+void print_rev(char *s)
+{
+
+	int r = 0
+
+	while (*str != '\0')
+	{
+		str++;
+		r++;
+	}
+	while (r > 0)
+	{
+	_putchar(*str);
+	r--;
+	}
+
+_putchar('\n');
+
+}
