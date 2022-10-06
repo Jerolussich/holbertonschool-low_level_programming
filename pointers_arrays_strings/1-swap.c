9b6b350db9b0651763090c21cafdd0325dd5b1a2
Jerolussich <jerolussich@hotmail.com>
2022-10-06 07:34:38 -0700

    task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - set number using pointer
+ * @n: stores a value
+ **/
+
+void swap_int(int *a, int *b);
+{
+	int *p = &*a;
+	int *p1 = &*b;
+
+	*p1 =*a;
+	*p  =*b;
+}
