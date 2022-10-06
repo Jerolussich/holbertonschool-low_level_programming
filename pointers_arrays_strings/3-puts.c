ce0e5a05e831a935f799382f1287ae976e6ccc27
Jerolussich <jerolussich@hotmail.com>
2022-10-06 08:36:47 -0700

    task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts - count a string
+ * @str: stores a value
+ * Return: returns sums of string
+ **/
+
+void _puts(char *str)
+{
+
+	while (*s != '\0')
+	{
+		_putchar(str);
+		s++;
+	}
+
+}
