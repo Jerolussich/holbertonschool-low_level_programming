f25426fef87a6b00de7988c58e0615b8a48f9602
Jerolussich <jerolussich@hotmail.com>
2022-10-07 04:56:23 -0700

    task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character
+ * @str: stores a value
+ * Return: returns sums of string
+ **/
+
+void puts2(char *str)
+{
+
+
+	while (*s != '\0')
+	{	
+		_putchar(*str);
+		s= s + 2
+	}
+
+	_putchar('\n');
+}
