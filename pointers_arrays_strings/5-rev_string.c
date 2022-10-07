eb01ea8029b21b21d083a867bef15175d877886f
Jerolussich <jerolussich@hotmail.com>
2022-10-07 04:56:16 -0700

    task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rev_string - print char in reverse
+ * @s: stores a value
+ * Return: returns sums of string
+ **/
+
+void rev_string(char *s)
+{
+
+	int add_index = 0;
+	char array[r];
+	int r = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		r++;
+	}
+		s--;
+
+	while (r > 0)
+	{
+		_putchar(*s);
+		add arry 
+		s--;
+		r--;
+	}
+
+	_putchar('\n');
+}
