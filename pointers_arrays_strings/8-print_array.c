4a497d01062c3248e66c300e5c7174b0906cfae8
Jerolussich <jerolussich@hotmail.com>
2022-10-07 08:26:00 -0700

    task 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,14 @@ void print_array(int *a, int n)
 	 int r = 0;
 
 
-	for (; n > r ; r++)
+	for (; n >= r ; r++)
 	{
-		printf("%d,", a[r]);
+		printf("%d, ", a[r]);
+		
+		if (n == r)
+		{
+			printf("%d", a[r];
+		}
 	}
 
 
