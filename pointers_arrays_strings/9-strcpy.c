6838bf6d32e97513094def41248cfb221c06f13a
Jerolussich <jerolussich@hotmail.com>
2022-10-07 09:46:10 -0700

    Task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcpy - copy the string to a buffer
+ * @*dest: pointer
+ * @*src: pointer
+ * Return: 0
+ **/
+
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+
+	return (*dest)
+}
