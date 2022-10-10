eaef0434dc33fbf159869e716739bd2c54e83e44
Jerolussich <jerolussich@hotmail.com>
2022-10-10 06:29:07 -0700

    task 0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -4,15 +4,16 @@
  * *_strcat : concatenate a string
  * @dest: stores a string
  * @src: stores a string
+ * Return: pointer to string dest
  **/
 
 char *_strcat(char *dest, char *src)
 {
-	
+
 	int r = 0;
 	int n = 0;
 
-	for(; dest[r] != '\0'; r++)
+	for (; dest[r] != '\0'; r++)
 	{
 	}
 	for (; src[n] != '\0'; n++)
