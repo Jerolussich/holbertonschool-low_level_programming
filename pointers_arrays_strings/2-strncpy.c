438d19c337f33e62b32e2fc026070045ec0b898d
Jerolussich <jerolussich@hotmail.com>
2022-10-11 04:57:17 -0700

    task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate a string
+ * @dest: stores a string
+ * @src: stores a string
+ * @n: stores a number of bytes
+ * Return: pointer to string dest
+ **/
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int i = 0;
+	int j = 0;
+
+	for (; dest[i]; i++)
+	{
+	}
+	for (; src[j] != src[n]; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+
+	return (dest);
+}
