ab184032fd0d895b3d62d4f740113ba9de84f753
Jerolussich <jerolussich@hotmail.com>
2022-10-11 08:38:25 -0700

    task 4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - reverse a string
+ * @s: pointer
+ * Return: 0
+ **/
+
+
+void reverse_array(int *a, int n)
+{
+
+	int n;
+	int temp = 0;
+	int i;
+
+
+	for (i = 0; i < (n /2); i++)
+	{
+		temp = s[i];
+		s[i] = s[n - i];
+		s[n] = temp;
+}
