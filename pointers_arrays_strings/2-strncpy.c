b3037e4377e29a1e7764882c996467536c4d9847
Jerolussich <jerolussich@hotmail.com>
2022-10-11 05:33:08 -0700

    task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,10 +10,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	
+	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[i] = src[j];
+		dest[i] = src[i];
 	}
 	for (; i < n; i++)
 	{	
