343de354380191f0f2255d1eaa923e2ad4528247
Jerolussich <jerolussich@hotmail.com>
2022-10-12 11:03:02 -0700

    task 0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memset - fill memory with constante byte
+ * @s: pointer
+ * @n: first n bytes to fill
+ * @b: constante byte
+ * Return: 0
+ **/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
