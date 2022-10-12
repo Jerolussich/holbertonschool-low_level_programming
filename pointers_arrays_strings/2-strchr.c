4b877e1d289b06cb78ff1135eb8dc29f60e08d59
Jerolussich <jerolussich@hotmail.com>
2022-10-12 11:37:46 -0700

    task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - fill memory with constante byte
+ * @dest: pointer
+ * @src: pointer
+ * @c: a character
+ * Return: dest
+ **/
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	for (;s != c ; s++)
+	{
+	}
+
+	return (s);
+}
