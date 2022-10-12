e479ac2bd1a71ac3344aef88e045b2807a8371d8
Jerolussich <jerolussich@hotmail.com>
2022-10-12 05:14:40 -0700

    task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * cap_string - reverse a string
+ * @s: pointer
+ * Return: 0
+ **/
+
+
+char *cap_string(char *dest)
+{
+	int  i;
+	int j;
+	char *s2 = {32, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125}
+
+	for (i = 0; dest[i] ; i++)
+	{
+		r = 0;
+
+		for (; s2[j] = '\0'; r++)
+		{
+			if (dest[i] = s2[j])
+			{
+				dest[i + 1] = dest[i] - 32;
+			}
+
+			r++;
+		}
+
+		i++;
+	}
+
+
+}
