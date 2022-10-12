d9d207c815077698ca1de907a7a24c4b8de39f7f
Jerolussich <jerolussich@hotmail.com>
2022-10-12 12:14:42 -0700

    task 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,14 @@ char *_strchr(char *s, char c)
 {
 	for (; *s != c && *s != '\0'; s++)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
-			
 	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	else
+	{
 		return ('\0');
+	}
 
 }
