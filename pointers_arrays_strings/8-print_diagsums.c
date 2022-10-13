f629739076377b0db52766621fe97d275aff7e3f
Jerolussich <jerolussich@hotmail.com>
2022-10-13 09:31:40 -0700

    task 8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -12,9 +12,18 @@ void print_diagsums(int *a, int size)
 	int sum = 0;
 	int sum1 = 0;
 
-	for(i = 0; i < size; i++)
+	for(i = 0; i < size*size; i++)
 	{
-		sum = sum + a[size - i][size - i];
-		sum1 = sum + a[size - i][i + 1];
+		if (i == 0)
+		{
+			sum = a[i];
+		}
+		else
+		{
+
+			sum = sum + a[i + (size - 1)];
+		}
 	}
+	printf("%d, ", sum);
+	printf("%d", sum1);
 }
