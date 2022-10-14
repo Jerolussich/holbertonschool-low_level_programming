9b04d512b2ca3282e6c0be3573353771a84b742b
Jerolussich <jerolussich@hotmail.com>
2022-10-14 13:42:57 -0700

    task 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -7,17 +7,13 @@
 int _strlen_recursion(char *s)
 {
 
-	int i = 0;
-
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		i++;
-		s++;
-		_strlen_recursion(s);
+		return (0);
 	}
-
-	return (i);
-
-
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
 
 }
