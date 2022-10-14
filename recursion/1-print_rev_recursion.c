243227ad1b558bc6d66b8320de23062f0782966c
Jerolussich <jerolussich@hotmail.com>
2022-10-14 08:47:44 -0700

    task 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -7,30 +7,13 @@
 void _print_rev_recursion(char *s)
 {
 
-int i = 0;
-int j = 0;
 
-	if (*s != '\0' && i != 1)
+	if (*s != '\0')
 	{
 		s++;
-		j++;
 		_print_rev_recursion(s);
-	}
-	if (*s == '\0')
-	{
-		i = 1;
-		s--;
 		_putchar(*s);
-		_print_rev_recursion(s);
 	}
-	else if (j >= 0)
-	{
-		_putchar(*s);
-		j--;
-		s--;
-		_print_rev_recursion(s);
-		
-	}
-
-
+	else
+		return;
 }
