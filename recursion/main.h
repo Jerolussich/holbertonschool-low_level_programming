25ac0a06dbe97fc86ea5d63967b07debb82b3953
Jerolussich <jerolussich@hotmail.com>
2022-10-16 07:19:31 -0700

    task 5

--- recursion/main.h
+++ recursion/main.h
@@ -6,6 +6,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int _sqrt_recursion2(int i, int k);
 int is_prime_number(int n);
 
 
