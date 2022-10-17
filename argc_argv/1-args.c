1c79fd4d4e131f2d5137e8c95676738feb6c01f5
Jerolussich <jerolussich@hotmail.com>
2022-10-17 12:03:20 -0700

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Print the number of arguments
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+
+	printf("%d", argc);
+	printf("\n");
+	return (0);
+}
+
