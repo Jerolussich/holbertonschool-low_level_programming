4c8ce1f31ff0ec1da09298d26c32e13df452995e
Jerolussich <jerolussich@hotmail.com>
2022-10-17 12:22:22 -0700

    task 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print all arguments it recieves
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	
+		printf("%s", (argv[i]));
+		printf("\n");
+	return (0);
+}
+
