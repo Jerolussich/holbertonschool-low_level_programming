73b8b17da8bd6fd8c55a7c9744456412122c9f80
Jerolussich <jerolussich@hotmail.com>
2022-10-17 13:58:26 -0700

    task 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - add positive numbers
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+	int j;
+
+	for (i = 1; argc > i; i++)
+	{
+		j = 0;
+
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] >= 'a' && 'z' >= argv[i][j])
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+		}
+		
+	
+		sum += atoi(argv[i]);
+		
+	}
+		printf("%d", sum);
+		printf("\n");
+		return (0);
+}
