0f880386f8bc8d34b20bf5fd5a4e6b6b0fe6b060
Jerolussich <jerolussich@hotmail.com>
2022-10-19 11:37:34 -0700

    task 0

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars and init with specific
+ * @size: size of array
+ * @c: char init
+ * Return: null or pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *p = NULL;
+	unsigned int i;
+	unsigned int j;
+
+	if (*str != '\0')
+	{
+		for (j = 0; str[j] != '\0'; j++)
+		{
+		}
+		
+		p = malloc(sizeof(char) * (j - 1));
+
+		if (p == NULL)
+		{
+			return (NULL);
+		}
+
+		for (i = 0; i < j; i++)
+		{
+			p[i] = str[i];
+		}
+		return (p);
+
+	}
+	return ('\0');
+
+}
