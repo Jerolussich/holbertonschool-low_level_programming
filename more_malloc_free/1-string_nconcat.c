b091fb4b0eb651d5d4c20df86ce9446d9a331fd4
Jerolussich <jerolussich@hotmail.com>
2022-10-21 10:01:08 -0700

    Task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -33,9 +33,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		p[j] = s1[j];
 	if (s2[0])
 	{
-		if (n < i)
+		for (k = 0; k < n; j++, k++)
 		{
-			for (k = 0; k < n; j++, k++)
 				p[j] = s2[k];
 		}
 	}
