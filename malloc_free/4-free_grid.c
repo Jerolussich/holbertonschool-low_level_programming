9d6b09b508b5c2fd76e83363978682e5696ee14e
Jerolussich <jerolussich@hotmail.com>
2022-10-21 04:48:01 -0700

    task 4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - free 2 dimensional grid.
+ * @width: int width array
+ * @height: int height array
+ i Return: null or pointer
+ */
+
+void free_grid(int **grid, int height);
+{
+
+	p[i] = malloc(sizeof(int) * width);
+		
+	if (p[i] == NULL)
+	{
+		for (i = i - 1; i >= 0; i--)
+		free(p[i]);
+	}
+	free(p);
+	return (NULL);
