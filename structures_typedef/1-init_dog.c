10e2dbf3d6032310d75182f2c06525bbc4858632
Jerolussich <jerolussich@hotmail.com>
2022-10-27 06:51:52 -0700

    task 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -11,8 +11,8 @@
 void init_dog(struct dog *d, char *name, float age, char *owner);
 {
 	dog = &my_dog;
-	dog->.name = name;
-	dog->.age = age;
-	dog->.owner = owner;
+	my_dog->.name = name;
+	my_dog->.age = age;
+	my_dog->.owner = owner;
 	return (0);
 }
