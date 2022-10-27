c1e25c91dac235ef6c94b81c7340f6dad791ab82
Jerolussich <jerolussich@hotmail.com>
2022-10-27 11:40:57 -0700

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - print structure
@@ -12,32 +13,31 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i = 0;
-	int j = 0;
 
 	dog_t *new_dog;
 
-	for (; name[i]; i++)
-		;
-	for (; owner[j]; j++)
-		;
-
 	new_dog = malloc(sizeof(dog_t));
-	new_dog->name = malloc(sizeof(char) * i);
-	new_dog->owner = malloc(sizeof(char) * j);
+	new_dog->name = strdup(name);
+	new_dog->owner = strdup(owner);
 
 	if (new_dog == NULL)
 	{
+		return (NULL);
+	}
+	new_dog->name = strdup(name);
+
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return(NULL);
+	}
+	new_dog->owner = strdup(owner);
+	if (new_dog->owner == NULL)
+	{
 		free(new_dog->name);
-		free(new_dog->owner);
 		free(new_dog);
 		return (NULL);
 	}
-	else
-	{
-		new_dog->name = name;
-		new_dog->owner = owner;
-		new_dog->age = age;
+	new_dog->age = age;
 		return (new_dog);
-	}
 }
