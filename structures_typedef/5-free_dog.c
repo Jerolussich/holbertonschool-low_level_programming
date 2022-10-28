f3abc12c24e1c62eb5ec61b1ba5d266dfe0b1c1b
Jerolussich <jerolussich@hotmail.com>
2022-10-28 05:16:01 -0700

    task 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free malloc
+ * @d: pointer
+ * Return: a null
+ */
+
+void free_dog(dog_t *d)
+{
+
+	if (d == NULL)
+	{
+		return (NULL);
+
+		if (new_d->name == NULL)
+		{
+			free(d);
+			return (NULL);
+		}
+		if (new_d->owner == NULL)
+		{
+			free(new_d->name);
+			free(new_d);
+			return (NULL);
+		}
+	}
+}
