b8c3c0ea8d30bf339af7510e72784b212e7ace8f
Jerolussich <jerolussich@hotmail.com>
2022-10-28 08:29:58 -0700

    task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - print a name 
+ * @name: string
+ * @f: pointer to function
+ * @
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	print_name(name, f);
+}
