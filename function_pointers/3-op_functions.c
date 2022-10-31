acb911e461d11778e7d2923c9e0423875229e070
Jerolussich <jerolussich@hotmail.com>
2022-10-31 07:56:55 -0700

    task 3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+/**
+ * op_add - function that adds a number
+ * @a: number a
+ * @b: number b
+ * Return: sum of a and b
+ **/
+int op_add(int a, int b)
+{
+	int c = 0;
+	c = a + b;
+	return (c);
+}
+/**
+ * op_sub - function that substracts a number
+ * @a:number a
+ * @b:number b
+ * Return: substraction of a and b
+ **/
+int op_sub(int a, int b)
+{
+	int c = 0;
+	c = a - b;
+	return (c);
+}
+/**
+ * op_mul - function that multiply
+ * @a: number a
+ * @b: number b
+ * Return: value of a and b multiplied
+ */
+int op_mul(int a, int b)
+{
+	int c = 0;
+	c = a * b;
+	return (c);
+}
+/**
+ * op_div - function that divides
+ * @a: number a
+ * @b: number b
+ * Return: value of a divided by b
+ */
+int op_div(int a, int b)
+{
+	int c = 0;
+	c = a / b;
+	return (c);
+}
+/**
+ * op_mod - function that gives remainder of a number
+ * @a: number a
+ * @b: number b
+ * Return: value of reminder of division of a and b
+ */
+int op_mod(int a, int b)
+{
+	int c = 0;
+	c = a % b;
+	return (c);
+}
