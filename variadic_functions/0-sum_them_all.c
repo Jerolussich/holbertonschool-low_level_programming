df2d4934ffa92203f106446167ec51d4f91ea9f7
Jerolussich <jerolussich@hotmail.com>
2022-11-01 07:00:17 -0700

    task

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 #include <stdarg.h>
-
+/**
+ * sum_them_all - sums all ints from a varaidic function
+ *
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
-
+	unsigned int i;
 	va_list pointer;
 
-	va_start(pointer, n)
-	sum += va_arg(ptr,in);
-	va_end(ptr);
-
+	va_start(pointer, n);
+	for (i = 0; i < n; i++)
+	{
+	sum += va_arg(pointer, unsigned int);
+	}
+	va_end(pointer);
 	return (sum);
+}
