88d0a24f3d7a3c5464814e0db634dbef4868c506
Jerolussich <jerolussich@hotmail.com>
2022-11-18 08:39:02 -0800

    task 1

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked list
+ * @h: pointer to a module with an int
+ * Return: modules found
+ */
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
