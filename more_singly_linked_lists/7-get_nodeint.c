a7ff3b6cd6c5f461ad24b252ecd239e57bca0440
Jerolussich <jerolussich@hotmail.com>
2022-11-19 15:51:26 -0800

    task 7

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - return a node 
+ * @head: singly list
+ * Return: head node data or 0 if singly list is empty
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *h = head;
+	unsigned int i = 0;
+
+	if (!h)
+		return (0);
+	while (h->next)
+	{
+		h = h->next;
+		i++;
+	}
+	if (i < index)
+	{
+		return (0);
+	}
+	return (h);
+}
