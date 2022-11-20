c575c97be13f943ca54ef6cd1da28a4405f26470
Jerolussich <jerolussich@hotmail.com>
2022-11-20 12:08:56 -0800

    task 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -4,12 +4,13 @@
  * get_bit - print binary representation of a number
  * @n: unsigned long int n
  * @index: place to check
+ * Return:  - 1 or int number
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index < 64)
-		return((n & (1 << index)) >> index);
+		return ((n & (1 << index)) >> index);
 	else
 		return (-1);
 }
