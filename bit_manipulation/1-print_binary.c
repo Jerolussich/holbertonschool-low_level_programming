f987a439d60abef9a453e287d8df43932d62fc13
Jerolussich <jerolussich@hotmail.com>
2022-11-20 11:25:57 -0800

    task 1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * print_binary - print binary representation of a number
+ * @b: binary number
+ * Return: value of int
+ */
+
+void print_binary(unsigned long int n)
+{
+	long i;
+
+	cout << "0";
+	for (i = 1 << 30; i > 0; i = i / 2)
+	{
+		if((n & i) != 0)
+		{
+			cout << "1";
+		}
+		else
+		{
+			cout << "0";
+		}
+	}
+}
