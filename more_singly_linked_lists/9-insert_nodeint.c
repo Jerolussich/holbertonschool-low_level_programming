bfe4465e6f1cf2245c9b47e8a5a3b1c7cdaa3ecd
Jerolussich <jerolussich@hotmail.com>
2022-11-19 18:20:34 -0800

    task 9

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+/**
+ *  insert_nodeint_at_index - return a node
+ * @head: singly liist
+ * @idx: position of node to be added
+ * Return: head node data or 0 if singly list is empty
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *h = *head;
+	listint_t *b = *head;
+	listint_t *pointerchain = NULL;
+	unsigned int j = 0;
+	unsigned int i = 0;
+
+	if (!h)
+	{
+		add_nodeint(head, n);
+		return (*head);
+	}
+
+	 while (h->next && i < idx)
+        {
+                h = h->next;
+		i++;
+        }
+	 while (b->next && j < (idx - 1))
+        {
+                b = b->next;
+                j++;
+        }
+	
+	pointerchain = malloc(sizeof(listint_t));
+	if (!pointerchain)
+	{
+		free(pointerchain);
+		return (NULL);
+	}
+	pointerchain->n = n;
+	pointerchain->next = h;
+	b->next = pointerchain;
+	
+	return (pointerchain);
+}
