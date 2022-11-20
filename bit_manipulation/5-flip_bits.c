f3a91ac3a59125e833be82d3089357db051e66f0
Jerolussich <jerolussich@hotmail.com>
2022-11-20 12:41:35 -0800

    task 5

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * flip_bits - function that returns the number of bits you would need to flip to get from one number to another.
+ * flip_bits - function that returns the number
+ * of bits you would need to flip to get from one number to another.
  * @n: unsigned long int n
  * @m: unsigned long int m
  * Return: number of bits
