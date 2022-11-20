bd620670d1d70c4ce65ed7c3ce27ae9186403b17
Jerolussich <jerolussich@hotmail.com>
2022-11-20 12:17:28 -0800

    task 3

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * set_bit - print binary representation of a number
+ * @n: unsigned long int n
+ * @index: place to check
+ * Return: return 1 or - 1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	{
+	unsigned long int num = (1 << index)
+	unsigned long int  h = *n;
+
+	if (n)
+	{
+		if (index <= 64)
+		{
+			*n = num | h;
+			return (1);
+		}
+	}
+	return (-1);
+	}
+}
