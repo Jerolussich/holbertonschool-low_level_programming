7fc45f884467d267bfb9bc6e9480883bf89961db
Jerolussich <jerolussich@hotmail.com>
2022-11-21 12:00:16 -0800

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -4,7 +4,7 @@
 /**
  * create_file - create file with certain content
  * @filename: pointer to file
- * @letters: letters to print
+ * @text_content: letters to add to file
  * Return: actual number of letters or 0
  */
 int create_file(const char *filename, char *text_content)
@@ -13,15 +13,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fp = open("filename.txt",O_WRONLY | O_CREAT, 600);
+	fp = open(filename, O_CREAT | O_WRONLY, 0700);
 	if (fp == -1)
-		return (-0);
-
+		return (0);
 	st = strlen(text_content);
-
 	wr = write(fp, text_content, st);
 	if (wr == -1)
-		return (-0);
+		return (0);
 	close(fp);
 	return (1);
 }
