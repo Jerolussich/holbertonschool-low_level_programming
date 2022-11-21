d1b42abedd6476919d9c6e4cf6b30a1350f1ee86
Jerolussich <jerolussich@hotmail.com>
2022-11-21 10:37:43 -0800

    task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,7 +12,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
-	if (letter == 0)
+	if (letters == 0)
 		return (-1);
 	fp = open(filename, O_RDONLY);
 	if (fp == - 1)
