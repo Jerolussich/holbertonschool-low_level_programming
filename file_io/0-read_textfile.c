6e81259b2738879deeb4b2d8901dfd297f4e5994
Jerolussich <jerolussich@hotmail.com>
2022-11-21 10:50:41 -0800

    task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -14,14 +14,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	fp = open(filename, O_RDONLY);
 	if (fp == -1)
-		return (-1);
+		return (-0);
 
 	buf = malloc(letters + 1);
 	if (!buf)
-		return (-1);
+		return (-0);
 	rd = read(fp, buf, letters);
 	if (rd == -1)
-		return (-1);
+		return (-0);
 	buf[letters + 1] = '\0';
 	close(fp);
 
