96b6e4715eef83ae74877722bc73b4dfcd10a46e
Jerolussich <jerolussich@hotmail.com>
2022-11-22 08:19:43 -0800

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -15,12 +15,12 @@ int main(int argc, char *argv[])
 	
 	if (argc != 3)/*if the number of argument is not the correct one, exit with code 97*/
 	{
-		printf("Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		return (97);
 	}
 	if (!argv[1])/*if file_from does not exist, exit with code 98*/
 	{
-		printf("Error: Can't read from file NAME_OF_THE_FILE\n");
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
 		return (98);
 	}
 	if (!argv[2])
@@ -31,38 +31,38 @@ int main(int argc, char *argv[])
 		pp = open(argv[2], O_WRONLY | O_TRUNC);
 	if (pp == -1)/*if failed to open pp*/
 	{
-		printf("Error: Can't write to NAME_OF_THE_FILE\n");
+		dprintf(STDERR_FILENO,"Error: Can't write to NAME_OF_THE_FILE\n");
 		return (99);
 	}
 	fp = open(argv[1], O_WRONLY);
 	if (fp == -1)/*if failed to open fp*/
 	{
-		printf("Error: Can't read from file NAME_OF_THE_FILE\n");
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
 		return (98);
 	}
 	rd = read(fp, buf, 1024);
 	if (rd == -1)/*if failed to read*/
 	{
-		printf("Error: Can't read from file NAME_OF_THE_FILE\n");
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
 		return (98);
 		/*if file_form can't not read it*/
 	}
 	if (fp == -1)/*if you can not create or if write to file_to fails, exit with code 99*/
 	{
-		printf("Error: Can't write to NAME_OF_THE_FILE\n");
+		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
 		return (99);
 	}
 	wr = write(pp, buf, 1024);
 	if (wr == -1)
 	{
-		printf("Error: Can't write to NAME_OF_THE_FILE\n");
+		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
 		return (99);
 	}
 	close(fp);
 	close(pp);
 	if (fp == -1 || pp == -1)/*if you can not close a file descriptor , exit with code 100*/
 	{
-		printf("Error: Can't close fd FD_VALUE");
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE");
 		return (100);
 	}
 	return (1);
