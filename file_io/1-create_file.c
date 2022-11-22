47e40f78ca089d2874afb4216672bebcb439276f
Jerolussich <jerolussich@hotmail.com>
2022-11-22 04:53:45 -0800

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,6 +13,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+	if (!textcontent)
+		return (1);
 	fp = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (fp == -1)
 		return (-1);
