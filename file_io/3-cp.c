269831ca5e05bc59e11ee81ed978d68aef388649
Jerolussich <jerolussich@hotmail.com>
2022-11-23 05:47:48 -0800

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,15 +16,17 @@ int main(int argc, char *argv[])
 		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
 	if (!argv[1])
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	}
 	pp = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (pp == -1)
 		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 	fp = open(argv[1], O_RDONLY);
 	if (fp == -1)
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	while ((rd = read(fp, buf, 1024)) != 0)
 	{
 		if (rd == -1)
