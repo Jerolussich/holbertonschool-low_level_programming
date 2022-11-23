d976e8420b0fb8c8101be5139c5097e7ddda8cf7
Jerolussich <jerolussich@hotmail.com>
2022-11-23 05:45:32 -0800

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,18 +9,21 @@
  */
 int main(int argc, char *argv[])
 {
-	int fp = 0,rd = 0, wr = 0, pp = 0;
+	int fp = 0, rd = 0, wr = 0, pp = 0;
 	char *buf[1024];
 
-	if (argc != 3)/*if the number of argument is not the correct one, exit with code 97*/
+	if (argc != 3)
 		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
-	if (!argv[1])/*if file_from does not exist, exit with code 98*/
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	pp = open(argv[2],O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	if (pp == -1)/*if failed to open pp*/
-		dprintf(2,"Error: Can't write to %s\n", argv[2]), exit(99);
+	if (!argv[1])
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	pp = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (pp == -1)
+		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 	fp = open(argv[1], O_RDONLY);
-	if (fp == -1)/*if failed to open fp*/
+	if (fp == -1)
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	while ((rd = read(fp, buf, 1024)) != 0)
 	{
