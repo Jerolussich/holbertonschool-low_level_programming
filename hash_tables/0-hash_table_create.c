1ffb03ab855e58c8eded329bf5f2ac35066e70d8
Jerolussich <jerolussich@hotmail.com>
2022-11-24 12:24:41 -0800

    task 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,8 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	hash_table_t *array;
-	unsigned long int i = 0;
+	hash_node_t *array;
 
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
