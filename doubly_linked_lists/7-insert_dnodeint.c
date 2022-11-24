5cf73e8cb328b7fe8a734ea917b370ea29b53f66
Jerolussich <jerolussich@hotmail.com>
2022-11-24 05:29:46 -0800

    task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,27 +15,24 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	pointerchain = malloc(sizeof(dlistint_t));
 	if (!pointerchain)
-	{
 		return (NULL);
-	}
 	pointerchain->n = n;
-	if (idx == 0)
-	{
-		pointerchain->next = h1;
-		pointerchain->prev = NULL;
-		*h = pointerchain;
-		return (pointerchain);
-	}
+
+	if (idx == 0 || *h == NULL)
+		return (add_dnodeint(h, n));
 	for (i = 0; i < idx - 1; i++)
 	{
-		if (h1->next == NULL)
+		if (!h1)
 			return (NULL);
 		h1 = h1->next;
 	}
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
 	pointerchain->next = h1->next;
 	pointerchain->prev = h1;
 	h1->next = pointerchain;
-	h1 = h1->next;
+	h1 = pointerchain->next;
 	h1->prev = pointerchain;
 	return (pointerchain);
 }
