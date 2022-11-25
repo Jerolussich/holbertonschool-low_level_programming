9bc8eb6003d3fa4b9544e9a8ef48062358201593
Jerolussich <jerolussich@hotmail.com>
2022-11-25 05:05:52 -0800

    task 2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,14 +1,12 @@
 #include "hash_tables.h"
 
 /**
- * key_index: provides the index of a key
- * @key - the key
- * @size - size of the array
- * Return - returns the index where key should be stored in the array
+ * key_index - provides the index of a key
+ * @key: the key
+ * @size: size of the array
+ * Return: returns the index where key should be stored in the array
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return(hash_djb2(key) % size);
+	return (hash_djb2(key) % size);
 }
-
-	
