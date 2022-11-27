a64a0e9fcde67d1a283cf9c3bddfe95ac3135794
Jerolussich <jerolussich@hotmail.com>
2022-11-27 11:53:28 -0800

    task 6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -20,10 +20,13 @@ void hash_table_delete(hash_table_t *ht)
 		while (pointerArray)
 		{
 			temp = pointerArray;
+			free(pointerArray->key);
+			free(pointerArray->value);
 			pointerArray = pointerArray->next;
 			free(temp);
 		}
 
 	}
 	free(ht);
+	free(ht->array);
 }
