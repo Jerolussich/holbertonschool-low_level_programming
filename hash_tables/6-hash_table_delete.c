cf48345ccb38167b95ac3553de811be26fa0daf8
Jerolussich <jerolussich@hotmail.com>
2022-11-27 11:29:11 -0800

    task 6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete hash table
+ * @ht: given hash table
+ * Return: no return
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int i = 0;
+	hash_node_t *pointerArray, *temp;
+
+	if (!ht)
+		return;
+
+	for (; i < ht->size; i++)
+	{
+		pointerArray = ht->array[i];
+
+		while (pointerArray)
+		{
+			temp = pointerArray;
+			pointerArray = pointerArray->next;
+			free(temp);
+		}
+
+	}
+}
