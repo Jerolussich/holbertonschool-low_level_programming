d53ea53f306cc1f58bc7d6868a65298d8575fc09
Jerolussich <jerolussich@hotmail.com>
2022-11-27 09:19:32 -0800

    task 5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -7,15 +7,14 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int i = 0, size;
+	unsigned long int i = 0;
 	hash_node_t *pointerArray;
 
-	size = ht->size;
 	pointerArray = ht->array[i];
 
 	if (ht)
 	{
-		while (pointerArray && i < size)
+		while (pointerArray)
 		{
 			printf("%s", pointerArray->key);
 			printf("%s", pointerArray->value);
