5c6133ed8fef878f332b35062ecbd98b74f38d96
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-05 15:59:00 -0300

    task 1 uploaded correctly

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -2,20 +2,32 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	int left = 0, right = size - 1, middle = size;
+	int left = 0, right = size - 1, middle = size, i;
 
 	if (array == NULL)
 		return (-1);
 
 	while (right >= left)
 	{
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			if (i == right)
+				printf("%d", array[i]);
+			else
+				printf("%d, ", array[i]);
+		}
+		printf("\n");
+		
     	middle = (right + left) / 2;
 		if (array[middle] == value)
 			return (middle);
-		else if (array[middle] > value)
-			right = middle - 1;
-		else
+		else if (array[middle] < value)
 			left = middle + 1;
+		else
+			right = middle - 1;
+		
+		
 	}
 	return (-1);
 }
